0dd937f0c738a3e5dbc90e4c0b1fabf409430901
Maciej Mrowiec <maciej.mrowiec@cfengine.com>
2013-08-30 19:17:42 +0200

    Fix: call diff generation before config cleanup

--- cf-agent/cf-agent.c
+++ cf-agent/cf-agent.c
@@ -274,9 +274,9 @@ int main(int argc, char *argv[])
 
     EndAudit(ctx, CFA_BACKGROUND);
     EvalContextDestroy(ctx);
-    GenericAgentConfigDestroy(config);
 
     GenerateDiffReports(config->input_file);
+    GenericAgentConfigDestroy(config);
 
     return ret;
 }
